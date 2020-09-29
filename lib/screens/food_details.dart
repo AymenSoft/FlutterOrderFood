7e06d8a19ae233375ec21092315111d1fbbbad3b
Aymen Masmoudi <aymen.msd@hotmail.fr>
2020-09-29 17:30:59 +0200

    add food details and control quantity

--- lib/screens/food_details.dart
+++ lib/screens/food_details.dart
@@ -15,6 +15,8 @@ class FoodDetails extends StatefulWidget {
 
 class _FoodDetailsState extends State<FoodDetails> {
 
+  int requestQuantity = 1;
+
   final heroTag;
   final name;
   final price;
@@ -39,7 +41,7 @@ class _FoodDetailsState extends State<FoodDetails> {
           },
           icon: Icon(Icons.arrow_back, color: Colors.white),
         ),
-        title: Text('Details'),
+        title: Text(name),
         centerTitle: true,
         actions: <Widget>[
           IconButton(
@@ -82,7 +84,7 @@ class _FoodDetailsState extends State<FoodDetails> {
               width: MediaQuery.of(context).size.width,
               child: Wrap(
                 alignment: WrapAlignment.center,
-                children: [
+                children: <Widget>[
                   Hero(
                     tag: widget.heroTag,
                     child: Container(
@@ -95,6 +97,111 @@ class _FoodDetailsState extends State<FoodDetails> {
                         ),
                       ),
                     ),
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              top: 250,
+              left: 25,
+              right: 25,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Price',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        widget.price,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 20,
+                          color: Colors.grey
+                        ),
+                      ),
+                      Container(
+                        height: 25,
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                      Container(
+                        height: 40,
+                        width: 125,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(17),
+                          color: Color(0xff7A9BEE)
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            InkWell(
+                              onTap: (){
+                                setState(() {
+                                  if(requestQuantity>1){
+                                    requestQuantity--;
+                                  }
+                                });
+                              },
+                              child: Container(
+                                height: 25,
+                                width: 25,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(7),
+                                  color: Color(0xff7A9BEE)
+                                ),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              requestQuantity.toString(),
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.white
+                              ),
+                            ),
+                            InkWell(
+                              onTap: (){
+                                setState(() {
+                                  requestQuantity++;
+                                });
+                              },
+                              child: Container(
+                                height: 25,
+                                width: 25,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(7),
+                                  color: Color(0xff7A9BEE)
+                                ),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   )
                 ],
               ),
